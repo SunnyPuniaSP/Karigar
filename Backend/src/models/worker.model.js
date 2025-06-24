b9fb5b08fe78dc36d4203f4c372173bbc4003245
SunnyPuniaSP <sunnypunia905@gmail.com>
2025-06-24 08:13:41 +0530

    Implement password hashing and token generation methods in worker model

--- Backend/src/models/worker.model.js
+++ Backend/src/models/worker.model.js
@@ -100,4 +100,42 @@ const workerSchema = new mongoose.Schema(
 
 workerSchema.index({ currentLocation: "2dsphere" });
 
+workerSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password =await bcrypt.hash(this.password, 10)
+    next()
+})
+
+workerSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+workerSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+workerSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 export const Worker = mongoose.model("Worker", workerSchema);
