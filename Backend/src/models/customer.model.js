d0f7e625ed384f82abba663eb9e41297447c2528
SunnyPuniaSP <sunnypunia905@gmail.com>
2025-06-24 08:11:22 +0530

    Add bcrypt and jsonwebtoken for password hashing and token generation in customer model

--- Backend/src/models/customer.model.js
+++ Backend/src/models/customer.model.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import { searchCharge } from "../constants";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const customerSchema = new mongoose.Schema({
     fullName: {
@@ -51,4 +53,41 @@ const customerSchema = new mongoose.Schema({
     }
 },{timestamps: true});
 
+customerSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password =await bcrypt.hash(this.password, 10)
+    next()
+})
+
+customerSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+customerSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+customerSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 export const Customer = mongoose.model("Customer", customerSchema);
