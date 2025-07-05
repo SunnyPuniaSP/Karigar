e2712408b4a07133355410b29f5a85c3e586f866
SunnyPuniaSP <sunnypunia905@gmail.com>
2025-07-05 18:15:32 +0530

    corrected file name in import

--- Backend/src/controllers/payment.controller.js
+++ Backend/src/controllers/payment.controller.js
@@ -1,7 +1,7 @@
 import razorpay from 'razorpay';
 import { asyncHandler } from '../utils/asyncHandler.js';
-import { ApiError } from '../utils/apiError.js';
-import { ApiResponse } from '../utils/apiResponse.js';
+import { ApiError } from '../utils/ApiError.js';
+import { ApiResponse } from '../utils/ApiResponse.js';
 import ServiceRequest from '../models/serviceRequest.model.js';
 import crypto from 'crypto';
 
