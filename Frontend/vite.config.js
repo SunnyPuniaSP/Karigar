be675d106a0b1d35682201a6cc6b3303adcc0ba2
SunnyPuniaSP <sunnypunia905@gmail.com>
2025-07-16 19:41:05 +0530

    modify vite config file to take backend url from env

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,22 +1,26 @@
-import path, { dirname } from "path"
+import path, { dirname } from "path";
 import { fileURLToPath } from "url";
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default defineConfig({
-  server:{
-    proxy:{
-      '/api':'http://localhost:8000',
-    }
-  },
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, path.resolve()); 
+
+  return {
+    server: {
+      proxy: {
+        "/api": env.VITE_API_URL,
+      },
     },
-  },
-})
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
