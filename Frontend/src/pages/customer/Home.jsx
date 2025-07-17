e9974532477d40a9b664a2e55b7fff3899299f83
SunnyPuniaSP <sunnypunia905@gmail.com>
2025-07-17 15:48:26 +0530

    fix worker home page name issue

--- Frontend/src/pages/customer/Home.jsx
+++ Frontend/src/pages/customer/Home.jsx
@@ -6,7 +6,7 @@ import { toast } from "sonner";
 import { useSelector } from "react-redux";
 const Home = () => {
   const navigate = useNavigate();
-  const { liveServiceId, isLiveRequest } = useSelector(
+  const { liveServiceId, isLiveRequest, fullName } = useSelector(
     (state) => state.customerAuth
   );
   const bookService = () => {
@@ -49,7 +49,7 @@ const Home = () => {
       <div className="flex justify-center items-center p-5 gap-5">
         <div className="w-1/2 flex flex-col gap-5">
           <h1 className="text-4xl md:text-5xl font-bold text-gray-900 mb-4">
-            Welcome back, <span className="text-blue-600">[Name]</span>!
+            Welcome back, <span className="text-blue-600">{fullName}</span>!
           </h1>
           <p className="text-lg text-gray-700 mb-8">
             Find skilled, verified technicians for all your home service
